6610c0053051d2864e19818cdcfcc00e396a75be
Lukas Forst <lukas.forst@blindspot.ai>
2017-11-06 19:26:07 +0100

    prepared collections

--- hw03/src/alg/Main.java
+++ hw03/src/alg/Main.java
@@ -1,5 +1,10 @@
 package alg;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -9,13 +14,14 @@ public class Main {
         Data result = reader.readData();
 
         System.out.println(result);
-//        MatrixSolver solver = new MatrixSolver(result);
-//        long startTime = System.currentTimeMillis();
-//        System.out.println(solver.solve());
+        /*MatrixSolver solver = new MatrixSolver(result);
+        long startTime = System.currentTimeMillis();
+        List<Integer> solved = solver.solve();
+        System.out.println(Arrays.toString(solved.toArray()));
 
-//        long endTime   = System.currentTimeMillis();
-//        long totalTime = endTime - startTime;
-//        System.out.println("Execution time: " + TimeUnit.MILLISECONDS.toSeconds(totalTime) + "s = " + totalTime + "ms");
-
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Execution time: " + TimeUnit.MILLISECONDS.toSeconds(totalTime) + "s = " + totalTime + "ms");
+*/
     }
 }
--- hw03/src/alg/MatrixSolver.java
+++ hw03/src/alg/MatrixSolver.java
@@ -1,13 +1,43 @@
 package alg;
 
+import java.util.*;
+
 public class MatrixSolver {
     private Data data;
+    private final Map<Integer, Set<Integer>> connections;
+    private final List<Integer> possibleSockets;
+
+    private final Set<Integer> visited;
+    private final List<Integer> sockets;
+    private final Queue<Integer> toVisit;
+
+    private boolean hasSolution = false;
 
     public MatrixSolver(Data data) {
         this.data = data;
+        connections = data.getConnections();
+        possibleSockets = data.getPossibleSockets();
+
+        int initialCapacity = data.getNumberOfNodes() / 2;
+        visited = new HashSet<>(data.getNumberOfNodes());
+        sockets = new ArrayList<>(initialCapacity);
+        toVisit = new PriorityQueue<>(initialCapacity);
     }
 
-    public int[] solve() {
-        return new int[1];
+    public List<Integer> solve() {
+        for (int node : possibleSockets) {
+            recursiveSolve(node, 0);
+
+            if (hasSolution) {
+                break;
+            }
+        }
+
+        Collections.sort(sockets);
+        return sockets;
+    }
+
+    private void recursiveSolve(int initialNode, int depth) {
+
     }
 }

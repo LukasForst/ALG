c0afe5bd0d07e7b8b929c633faf93991ea2a29bf
Lukas Forst <lukas.forst@blindspot.ai>
2017-11-09 18:36:05 +0100

    wip

--- hw03/src/alg/MatrixSolver.java
+++ hw03/src/alg/MatrixSolver.java
@@ -1,5 +1,6 @@
 package alg;
 
+import java.nio.channels.Pipe;
 import java.util.*;
 
 public class MatrixSolver {
@@ -7,9 +8,9 @@ public class MatrixSolver {
     private final Map<Integer, Set<Integer>> connections;
     private final List<Integer> possibleSockets;
 
-    private final Map<Integer, Integer> visited;
+    private final Map<Integer, Pair> visited;
     private final List<Integer> sockets;
-    private final Queue<Integer> toVisit;
+    private final Queue<Pair> toVisit;
 
     private boolean hasSolution = false;
 
@@ -26,44 +27,102 @@ public class MatrixSolver {
 
     public List<Integer> solve() {
         for (int node : possibleSockets) {
-            if (recursiveSolve(node, 0) == RecursiveStatus.SOLVED) {
+            sockets.add(node);
+            int array[] = connections.get(node).stream().mapToInt(Integer::valueOf).toArray();
+            toVisit.add(new Pair(array[0], 0, Direction.LEFT));
+            toVisit.add(new Pair(array[1], 0, Direction.RIGHT));
+            if(recursiveSolve(toVisit.poll()) == RecursiveStatus.SOLVED)
                 break;
-            }
+
+            toVisit.clear();
+            sockets.clear();
         }
+
 
         Collections.sort(sockets);
         return sockets;
     }
 
-    private RecursiveStatus recursiveSolve(int initialNode, int depth) {
-        if (visited.containsKey(initialNode)) {
-            if (visited.get(initialNode) != depth) {
-                sockets.clear();
-                visited.clear();
+    private RecursiveStatus recursiveSolve(Pair initialNode) {
+        if (visited.containsKey(initialNode.getValue())) {
+            Pair contained = visited.get(initialNode.getValue());
+            if(contained.getDepth() == initialNode.getDepth() && contained.getDirection() != initialNode.getDirection()){
+                sockets.add(initialNode.getValue());
+                recursiveSolve(toVisit.poll());
+            } else{
                 return RecursiveStatus.WRONG_NODE;
-            } else {
-                sockets.add(initialNode);
-                if (toVisit.peek() != null) {
-                    return recursiveSolve(toVisit.poll(), ++depth);
-                } else {
-                    return RecursiveStatus.SOLVED;
-                }
             }
         } else {
-            visited.put(initialNode, depth);
+            visited.put(initialNode.getValue(), initialNode);
         }
 
-        toVisit.addAll(connections.get(initialNode));
-        toVisit.remove(initialNode);
+        for (int nodeValue : connections.get(initialNode.getValue())) {
+            if (!toVisit.contains(new Pair(nodeValue, initialNode.getDepth() - 1, initialNode.getDirection()))) {
+                toVisit.add(new Pair(nodeValue, initialNode.getDepth() + 1, initialNode.getDirection()));
+            }
+        }
 
         if (toVisit.peek() == null) {
             return RecursiveStatus.SOLVED;
         } else {
-            return recursiveSolve(toVisit.poll(), ++depth);
+            return recursiveSolve(toVisit.poll());
         }
     }
 
     private enum RecursiveStatus {
         WRONG_NODE, SOLVED, NEXT_NODE
+    }
+}
+
+enum Direction {
+    LEFT, RIGHT;
+
+    public static Direction getOposite(Direction direction) {
+        if (direction == RIGHT) return LEFT;
+        else return RIGHT;
+    }
+}
+
+class Pair {
+    private final int value;
+    private final int depth;
+    private final Direction direction;
+
+    public Pair(int value, int depth, Direction direction) {
+        this.value = value;
+        this.depth = depth;
+        this.direction = direction;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair pair = (Pair) o;
+
+        if (value != pair.value) return false;
+        if (depth != pair.depth) return false;
+        return direction == pair.direction;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = value;
+        result = 31 * result + depth;
+        result = 31 * result + (direction != null ? direction.hashCode() : 0);
+        return result;
     }
 }
